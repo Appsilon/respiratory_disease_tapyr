6dec4eb17a8778f0353055d8e77a8eb86613276c
Piotr Pasza Storozenko <piotr@appsilon.com>
2024-04-29 15:10:44 +0200

    refactor: change project name and install dependencies

--- tests/pytest/test_utils.py
+++ tests/pytest/test_utils.py
@@ -1,6 +1,6 @@
 import pytest
 
-from tapyr_template.logic.utils import divide
+from respiratory_disease_tapyr.logic.utils import divide
 from tests.helpers.logging_helpers import log_contain_message
 
 
