23167229c9f6878d3467d3736f85be1672f0f7cf
Piotr Pasza Storozenko <piotr@appsilon.com>
2024-04-30 12:25:04 +0200

    feat: add app startup test

--- tests/playwirght/test_ui.py
+++ tests/playwirght/test_ui.py
@@ -2,6 +2,6 @@ from conftest import APP_URL
 from playwright.sync_api import Page, expect
 
 
-def test_footer(page: Page):
+def test_startup(page: Page):
     page.goto(APP_URL)
-    expect(page.get_by_test_id("docs-link")).to_contain_text("Start with the docs!")
+    expect(page).not_to_have_title("404")
